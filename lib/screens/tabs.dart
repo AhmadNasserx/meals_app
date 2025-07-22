f7514cb9b910c952002de3cf5a9e41cab01fb86f
AhmadNasserx <111495732+AhmadNasserx@users.noreply.github.com>
2025-07-22 10:27:25 +0300

    small fix

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -22,7 +22,7 @@ class _TabsScreenState extends State<TabsScreen>{
     Widget activePage = const CategoriesScreen();
     var activePageTitle = 'Categories';
     if (_selectedPageIndex == 1){
-      activePage = MealsScreen(title: 'Favorites', meals: []);
+      activePage = MealsScreen(meals: []);
       activePageTitle = 'Favorites';
     }
 
@@ -33,6 +33,7 @@ class _TabsScreenState extends State<TabsScreen>{
       body: activePage,
       bottomNavigationBar: BottomNavigationBar(
         onTap: _selectPage,
+        currentIndex: _selectedPageIndex,
         items: const [
           BottomNavigationBarItem(icon: Icon(Icons.set_meal) , label: 'Categories'),
           BottomNavigationBarItem(icon: Icon(Icons.star) , label: "Favorites"),
