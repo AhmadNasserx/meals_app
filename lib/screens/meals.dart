ee5561c8155962f775e02c6a3ff7748ce52403c9
AhmadNasserx <111495732+AhmadNasserx@users.noreply.github.com>
2025-07-22 10:59:54 +0300

    fixed favorite

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -4,14 +4,24 @@ import 'package:meals_app/screens/meal_details.dart';
 import 'package:meals_app/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
-  const MealsScreen({super.key, this.title, required this.meals});
+  const MealsScreen({
+    super.key,
+    this.title,
+    required this.meals,
+    required this.onToggleFavorite,
+  });
 
   final String? title;
   final List<Meal> meals;
-  void selectMeal(BuildContext context, Meal meal){
-    Navigator.of(context).push(MaterialPageRoute(builder: (ctx)=> MealDetailsScreen(meal: meal),),);
+  final void Function(Meal meal) onToggleFavorite;
+  void selectMeal(BuildContext context, Meal meal) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) =>
+            MealDetailsScreen(meal: meal, onToggleFavorite: onToggleFavorite),
+      ),
+    );
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -43,10 +53,13 @@ class MealsScreen extends StatelessWidget {
     } else {
       content = ListView.builder(
         itemCount: meals.length,
-        itemBuilder: (ctx, index) => MealItem(meal: meals[index], onSelectMeal: (context, meal) => selectMeal(context, meal),)
+        itemBuilder: (ctx, index) => MealItem(
+          meal: meals[index],
+          onSelectMeal: (context, meal) => selectMeal(context, meal),
+        ),
       );
     }
-    if (title == null){
+    if (title == null) {
       return content;
     }
     return Scaffold(
--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -1,43 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/models/meal.dart';
 import 'package:meals_app/screens/categories.dart';
 import 'package:meals_app/screens/meals.dart';
-class TabsScreen extends StatefulWidget{
-const TabsScreen({super.key});
 
-@override
+class TabsScreen extends StatefulWidget {
+  const TabsScreen({super.key});
+
+  @override
   State<TabsScreen> createState() {
     return _TabsScreenState();
   }
 }
-class _TabsScreenState extends State<TabsScreen>{
-  int _selectedPageIndex = 0;
 
-  void _selectPage(int index){
+class _TabsScreenState extends State<TabsScreen> {
+  int _selectedPageIndex = 0;
+  final List<Meal> _favoriteMeals = [];
+
+  void _toggleMealFavoriteStatus(Meal meal) {
+    final isExisting = _favoriteMeals.contains(meal);
+    if (isExisting) {
+      setState(() {
+        _favoriteMeals.remove(meal);
+      });
+    } else {
+      setState(() {
+          _favoriteMeals.add(meal);
+      });
+    }
+  }
+
+  void _selectPage(int index) {
     setState(() {
       _selectedPageIndex = index;
     });
   }
+
   @override
   Widget build(BuildContext context) {
-    Widget activePage = const CategoriesScreen();
+    Widget activePage = CategoriesScreen(
+      onToggleFavorite: _toggleMealFavoriteStatus,
+    );
     var activePageTitle = 'Categories';
-    if (_selectedPageIndex == 1){
-      activePage = MealsScreen(meals: []);
+    if (_selectedPageIndex == 1) {
+      activePage = MealsScreen(
+        meals: _favoriteMeals,
+        onToggleFavorite: _toggleMealFavoriteStatus,
+      );
       activePageTitle = 'Favorites';
     }
 
     return Scaffold(
-      appBar: AppBar(
-          title: Text(activePageTitle),
-      ),
+      appBar: AppBar(title: Text(activePageTitle)),
       body: activePage,
       bottomNavigationBar: BottomNavigationBar(
         onTap: _selectPage,
         currentIndex: _selectedPageIndex,
         items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.set_meal) , label: 'Categories'),
-          BottomNavigationBarItem(icon: Icon(Icons.star) , label: "Favorites"),
-
+          BottomNavigationBarItem(
+            icon: Icon(Icons.set_meal),
+            label: 'Categories',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.star), label: "Favorites"),
         ],
       ),
     );
